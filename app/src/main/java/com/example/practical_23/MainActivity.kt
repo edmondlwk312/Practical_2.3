ba3d2ea7ebe73f0be96b559003d113cd76640a38
edmondlwk312 <laiwk-wm17@student.tarc.edu.my>
2019-10-31 11:04:28 +0800

    Practical_2.3

--- app/src/main/java/com/example/practical_23/MainActivity.kt
+++ app/src/main/java/com/example/practical_23/MainActivity.kt
@@ -6,16 +6,18 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setListeners()
     }
 
-    private fun makeColored(view: View) {
+    private fun makeColored(view: View)
+    {
         when (view.id) {
 
             // Boxes using Color class colors for the background
@@ -24,12 +26,13 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
 
-    private fun setListeners() {
-
+    private fun setListeners()
+    {
         val boxOneText = findViewById<TextView>(R.id.box_one_text)
         val boxTwoText = findViewById<TextView>(R.id.box_two_text)
         val boxThreeText = findViewById<TextView>(R.id.box_three_text)
@@ -44,7 +47,8 @@ class MainActivity : AppCompatActivity() {
                 boxFourText, boxFiveText, rootConstraintLayout
             )
 
-        for (item in clickableViews) {
+        for (item in clickableViews)
+        {
             item.setOnClickListener { makeColored(it) }
         }
     }
